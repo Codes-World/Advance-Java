aa916d52bb3398d4b4f1d91b5537890ad7cbdf76
Codes-World <aman.soni6200@gmail.com>
2025-03-30 10:05:28 -0700

    Work flow

---
+++ Servlet/SessionListenerApp/src/main/java/in/codeworld/aman/listener/Dog.java
@@ -0,0 +1,25 @@
+package in.codeworld.aman.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+
+/**
+ * Application Lifecycle Listener implementation class Dog
+ *
+ */
+@WebListener
+public class Dog implements HttpSessionBindingListener {
+
+    public void valueBound(HttpSessionBindingEvent event)  { 
+    	System.out.println("Dog object added to session scope...");
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueUnbound(HttpSessionBindingEvent)
+     */
+    public void valueUnbound(HttpSessionBindingEvent event)  { 
+    	System.out.println("Dog object removed from session scope...");
+    }
+	
+}
