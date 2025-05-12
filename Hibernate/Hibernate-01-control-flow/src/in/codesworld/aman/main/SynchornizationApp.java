d99e420ec40a27e6fe7d3487bf9518edb96186e2
Codes-World <aman.soni6200@gmail.com>
2025-05-12 11:45:28 +0530

    Modified

---
+++ Hibernate/Hibernate-01-control-flow/src/in/codesworld/aman/main/SynchornizationApp.java
@@ -0,0 +1,43 @@
+package in.codesworld.aman.main;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import in.codesworld.aman.bean.Student;
+import in.codesworld.aman.util.HibernateUtil;
+
+public class SynchornizationApp {
+
+	public static void main(String[] args) {
+		Session session = null;
+		Student student = null;
+
+		try {
+			session = HibernateUtil.getSession();
+
+			student = session.get(Student.class, 3);
+			System.out.println("Before modification.....");
+			System.out.println("Record with the id value :: " + student);
+			
+			//application is in pausing state
+			System.in.read();
+
+			//synchronization established b/w record to java object
+			session.refresh(student);
+			
+			System.out.println("After modification....");
+			System.out.println("Record with the id value :: "+student);
+			
+		} catch (HibernateException he) {
+			he.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			HibernateUtil.closeSessionFactory();
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+}
