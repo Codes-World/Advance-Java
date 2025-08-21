79b88d22666e43c4a9eca4bb40d348fc3277c183
Codes-World <aman.soni6200@gmail.com>
2025-08-21 15:10:56 +0530

    TBSC

---
+++ Hibernate/Hibernate-26-InheritanceMapping-TBSC/src/in/codesworld/aman/main/TBSInsertApp.java
@@ -0,0 +1,56 @@
+package in.codesworld.aman.main;
+
+import java.time.LocalDate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.codesworld.aman.bean.CardPayment;
+import in.codesworld.aman.bean.ChequePayment;
+import in.codesworld.aman.util.HibernateUtil;
+
+public class TBSInsertApp {
+
+	public static void main(String[] args) {
+
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;
+		try {
+			session = HibernateUtil.getSession();
+			transaction = session.beginTransaction();
+
+			CardPayment cardPayment = new CardPayment();
+			cardPayment.setCardNo(1234L);
+			cardPayment.setCardType("debit");
+			cardPayment.setPaymentGateWay("visa");
+			cardPayment.setAmount(8000.0f);
+
+			session.save(cardPayment);
+
+			ChequePayment chequePayment = new ChequePayment();
+			chequePayment.setAmount(9000.0f);
+			chequePayment.setChequeNo(56456L);
+			chequePayment.setChequeType("all");
+			chequePayment.setExpriyDate(LocalDate.of(2025, 10, 25));
+
+			session.save(chequePayment);
+			flag = true;
+
+		} catch (HibernateException he) {
+			he.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Record saved to database...");
+			} else {
+				transaction.rollback();
+				System.out.println("Record not saved to database...");
+			}
+		}
+
+	}
+}
