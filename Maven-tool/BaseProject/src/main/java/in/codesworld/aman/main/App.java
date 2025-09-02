0da982881d86ae4c6d02d489372372b92732f2ea
Codes-World <aman.soni6200@gmail.com>
2025-09-02 09:23:19 +0530

    Multi-maven

---
+++ Maven-tool/BaseProject/src/main/java/in/codesworld/aman/main/App.java
@@ -0,0 +1,10 @@
+package in.codesworld.aman.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
