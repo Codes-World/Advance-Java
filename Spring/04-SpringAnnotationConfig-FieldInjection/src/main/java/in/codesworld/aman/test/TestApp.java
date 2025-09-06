d792e983312fcc9ad1835e87a3d804c854514232
Codes-World <aman.soni6200@gmail.com>
2025-09-06 21:05:45 +0530

    Field Injection

---
+++ Spring/04-SpringAnnotationConfig-FieldInjection/src/main/java/in/codesworld/aman/test/TestApp.java
@@ -0,0 +1,23 @@
+package in.codesworld.aman.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import in.codesworld.aman.bean.Employee;
+import in.codesworld.aman.config.AppConfig;
+
+public class TestApp {
+	public static void main(String[] args) {
+		
+		//Starting the container
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		//Working with bean created by IOC
+		Employee employee = context.getBean("employee", Employee.class);
+		System.out.println(employee);
+		
+		//close the container
+		((AbstractApplicationContext) context).close();
+	}
+}
