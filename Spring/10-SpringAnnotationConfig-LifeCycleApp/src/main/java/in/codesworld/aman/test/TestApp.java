a4ec9ec5ffc9c917c66cc03b121fa5b5a0b7963a
Codes-World <aman.soni6200@gmail.com>
2025-11-08 08:39:28 +0530

    Spring-Annotation-Config-LifeCycle

---
+++ Spring/10-SpringAnnotationConfig-LifeCycleApp/src/main/java/in/codesworld/aman/test/TestApp.java
@@ -0,0 +1,34 @@
+package in.codesworld.aman.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import in.codesworld.aman.bean.VoterVerifier;
+import in.codesworld.aman.config.AppConfig;
+
+public class TestApp {
+	public static void main(String[] args) {
+		
+		System.out.println("*****************Starting the container*****************");
+		//Starting the container
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		System.out.println("******Beans created and stored in IOCCache******");
+		String[] names = context.getBeanDefinitionNames();
+		for (String beanName : names) {
+			System.out.println(beanName);
+		}
+		
+		System.out.println("************************************************");
+		// Working with bean created by IOC
+		VoterVerifier bean = context.getBean("voter", VoterVerifier.class);
+		String status = bean.checkVotingEligibility();
+		System.out.println(status);
+
+		
+		//close the container
+		((AbstractApplicationContext) context).close();
+		System.out.println("*****************Closing the container*****************");
+	}
+}
