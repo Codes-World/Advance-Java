a545d9d1659fff17ffef977ff4fb1a3af979f380
Codes-World <aman.soni6200@gmail.com>
2025-11-08 08:41:19 +0530

    Spring-Annotation-Config-SPELEX

---
+++ Spring/11-SpringAnnotationConfig-SPELEX/src/main/java/in/codesworld/aman/test/TestApp.java
@@ -0,0 +1,26 @@
+package in.codesworld.aman.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import in.codesworld.aman.bean.Employee;
+import in.codesworld.aman.config.AppConfig;
+
+public class TestApp {
+	public static void main(String[] args) {
+		
+		System.out.println("*****************Starting the container*****************");
+		//Starting the container
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		// Working with bean created by IOC
+		Employee employee = context.getBean("employee", Employee.class);
+		System.out.println(employee);
+
+		
+		//close the container
+		((AbstractApplicationContext) context).close();
+		System.out.println("*****************Closing the container*****************");
+	}
+}
